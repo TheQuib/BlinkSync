5566d3cd6a73b0109da35be8fb3a8b7be56412cb
Quinn Henry <19qhenry@gmail.com>
2023-07-12 23:39:27 -0400

    Move files to subdirectory (project)

--- inkfi/boot.py
+++ inkfi/boot.py
@@ -1,10 +1,14 @@
-# This is script that run when device boot up or wake from sleep.
+import network
 
+wifi_ssid = "YOUR_WIFI_SSID"
+wifi_password = "YOUR_WIFI_PASSWORD"
 
-import main
-from main import epd2in13b as epd3in13b
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect(wifi_ssid, wifi_password)
 
-epd = epd3in13b()
-epd.init()
-epd.Clear()
-epd.sleep()
+while not sta_if.isconnected():
+    pass
+
+print("Connected to Wi-Fi:", wifi_ssid)
+print("IP address:", sta_if.ifconfig()[0])
